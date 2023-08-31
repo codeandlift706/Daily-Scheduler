f4835a1ba555400ca40eac4b15ff38fb6454c2fc
Code Queen <channguyen97@gmail.com>
2023-08-30 21:08:34 -0700

    adjusted listener function

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
-//gobal variables
-var saveButton = document.querySelector(".saveBtn");
-var textContainer = document.querySelector(".description");
-var inputText = document.querySelector(".inputText");
-
-//functions
-
-
-//processes
+var buttonEl = $('.saveBtn');
+var textContainerEl = $('.description');
 
 
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
@@ -19,17 +12,22 @@ $(function () {
   // HINT: What does `this` reference in the click listener function? 
   // How can DOM traversal be used to get the "hour-x" id of the time-block containing the button that was clicked? 
   // How might the id be useful when saving the description in local storage?
-  //
-  saveButton.addEventListener("click", saveText);
   
-    function saveText() {
-    localStorage.setItem('ItemToDo', textContainer.value); //store it in our localStorage
-    //textContainer.value = localStorage.getItem('ItemToDo');
+    //$('.saveBtn').click(function() { //assigns a click event function to all elements with .saveBtn class
+    buttonEl.click(function() {
+    //$('textarea').each(function() { //get all textarea elements, go through each one
+    textContainerEl.each(function() {
+    var value = $(this).val(); //this means, the textarea in the function. We are getting the value of it and storing it into the value variable.
+    var name = $(this).attr('#hour-9'); //In the example, name should be the key that is stored in localStorage. You'll need to figure what key values you'll use to get and set the textarea values. You will need to update the right side of the = to get what you want. .attr('') targets an attribute on the element (in this case textarea) and grabs the value of that attribute you put in there.
     
-    //var userInput = document.createElement('p');
-    //userInput.value 
-    //textContainer.append(userInput.value);
-  }
+    console.log(value) //console logs the value
+    localStorage[name] = value;
+    //localStorage[name] = value; //this stores the key and value into local storage
+    });   
+    });
+
+
+  })
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
@@ -43,4 +41,3 @@ $(function () {
   // TODO: Add code to display the current date in the header of the page.
   const currentDate = dayjs();
   $('#currentDay').text(currentDate.format('dddd, MMMM D YYYY'));
-});
